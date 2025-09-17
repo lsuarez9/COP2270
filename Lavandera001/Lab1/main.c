5d46dacd38b4c284cd71708517147ce35e960e19
unknown <Reynel.Lavandera001@mymdc.net>
2025-09-16 20:57:42 -0400

    Adding my first lab

---
+++ Lavandera001/Lab1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int main()
+{
+    printf("Lab1 Welcome!\n");
+
+
+    int max_int = INT_MAX;
+    int min_int = INT_MIN;
+
+
+
+    printf("Never add code below this line!\n");
+    return 0;
+}
