f42dffe21c41052a203d62d7ae3fdef3c1acaa7d
Suarez Cabrera <lsuarez9@mdc.edu>
2025-09-16 20:57:40 -0400

    Adding my first lab

---
+++ lsuarez9/Lab1/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main()
+{
+    printf("Lab1 welcome!\n");
+
+    int max_int = INT_MAX;
+    int min_int = INT_MIN;
+
+    printf("Maximum value of int: %d\n", max_int);
+    printf("Minimum value of int: %d\n", min_int);
+
+
+    printf("Never add code below this line!\n");
+    return 0;
+}
