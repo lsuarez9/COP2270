405f3f4b9ee0e3a8bd3599ae3087bcd1acf83c18
lavandera001 <lavandera58@gmail.com>
2025-10-07 20:13:08 -0400

    Removed extra files

---
+++ Merino002/Lab1/Lab1_Implementation.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Lab1\n");
+    
+    int a = 10; 
+    float b = 3.2; 
+    float difference = a - b;
+    
+    char ch = 'C';
+    int product = ch * a;
+    
+    printf("int - float = %f\n", difference);
+    printf("char * int = %d\n", product);
+
+    return 0;
+}
